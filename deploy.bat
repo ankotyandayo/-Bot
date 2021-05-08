24350c6fca6a6f3f1aeec4118585dcc107aab825
shuto <shuto.ando1@gmail.com>
2021-05-08 12:26:46 +0900

    'Improve'

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,2 @@
-#!/bin/sh
 
 git add . && git commit -m 'Improve' && git push
--- index.js
+++ index.js
@@ -30,16 +30,14 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
     // イベントオブジェクトを順次処理。
     req.body.events.forEach((event) => {
         // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
-        if (event.type == "message" && event.message.type == "text"){
-            // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
-            if (event.message.text == "こんにちは"){
+        
                 // replyMessage()で返信し、そのプロミスをevents_processedに追加。
                 events_processed.push(bot.replyMessage(event.replyToken, {
                     type: "text",
-                    text: "これはこれは"
+                    text: "僕は可愛いお天気カエルだよん\nまだ何もできない無能なカエル\n頑張って色々覚えるから(´;ω;｀)\nもう少し待ってね"
                 }));
             }
-        }
+        
     });
 
     // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
